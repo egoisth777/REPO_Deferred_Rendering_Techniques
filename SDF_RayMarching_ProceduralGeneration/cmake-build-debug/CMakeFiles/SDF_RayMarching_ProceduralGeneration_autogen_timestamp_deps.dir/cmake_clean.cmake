93ed10ef8d3f5ee468f26c2c95b2ecdc9bbe3662
Yueyang Li <egoistheresy@outlook.com>
2024-04-30 12:26:24 -0400

    try to fix the problem

--- SDF_RayMarching_ProceduralGeneration/cmake-build-debug/CMakeFiles/SDF_RayMarching_ProceduralGeneration_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SDF_RayMarching_ProceduralGeneration_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SDF_RayMarching_ProceduralGeneration_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- SDF_RayMarching_ProceduralGeneration/cmake-build-release/CMakeFiles/SDF_RayMarching_ProceduralGeneration.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for SDF_RayMarching_ProceduralGeneration.
-# This may be replaced when dependencies are built.
